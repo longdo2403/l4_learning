e36bfe6ff53bf23cbbde511aaee76c40439d6c13
Long Do <Administrator@10.190.201.12>
2015-04-29 17:01:53 +0700

    # Fix

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends BaseController {
      * Construct function
      */
     public function __construct(){
-        // Filter csrf attack
+        //Filter csrf attack
         $this->beforeFilter('csrf', array('on'=>'post'));
         //Create message object
         $this->message = new MessageHelper();
@@ -40,6 +40,7 @@ class HomeController extends BaseController {
             $params = Input::all();
             $validator = Validator::make($params, $this->rules);
             if ($validator->fails()) {
+                //Redirect previous page with validation errors
                 return Redirect::back()
                                 ->withInput()
                                 ->withErrors($validator);
@@ -48,16 +49,17 @@ class HomeController extends BaseController {
                 if ($ret){
                     $user = User::where('username', Input::get('username'))->first();
                     Auth::login($user);
-                    
                     //create message
                     $this->message->setType('success');
                     $this->message->setMess('Login OK !');
+                    //Redirect to member page with success message
                     return Redirect::route('member.index')
                                     ->with('message', $this->message->create());
                 } else {
                     //create message
                     $this->message->setType('danger');
                     $this->message->setMess('Login Failed !');
+                    //Redirect previous page with error message
                     return Redirect::back()
                                     ->withInput()
                                     ->with('message', $this->message->create());
